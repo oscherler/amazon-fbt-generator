5792f0dcf99fa3b6076e5d0d06c7eed4ac128308
Olivier Scherler <oscherler@femto-byte.com>
2016-08-02 11:22:51 +0200

    Added translation through Symfony Twig Bridge.

--- main.php
+++ main.php
@@ -2,8 +2,21 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\MessageSelector;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
+use Symfony\Bridge\Twig\Extension\TranslationExtension;
+
 $loader = new Twig_Loader_Filesystem( __DIR__ . '/views' );
 $twig = new Twig_Environment( $loader );
+
+$translator = new Translator( 'en', new MessageSelector() );
+$yaml_loader = new YamlFileLoader();
+
+$translator->addLoader( 'yaml', $yaml_loader );
+$translator->addResource( 'yaml', __DIR__ . '/translations/messages.en.yml', 'en' );
+
+$twig->addExtension( new TranslationExtension( $translator ) );
 
 $items = array(
 	array(
